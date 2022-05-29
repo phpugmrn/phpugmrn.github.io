1695cc24247603fa08ae673eff30791e2e8ba7ee
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2022-05-29 09:37:33 +0200

    Add GitHub CNAME config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,7 @@ const config = {
   organizationName: 'phpugmrn', // Usually your GitHub org/user name.
   projectName: 'phpugmrn.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
