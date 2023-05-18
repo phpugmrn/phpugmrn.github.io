f24ebee8ea920df0617fc265b9b3f02964d8b1d3
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2023-05-18 08:53:14 +0200

    Fix build problem

--- src/pages/raffler.js
+++ src/pages/raffler.js
@@ -1,110 +1,116 @@
-import React, { useState } from 'react'
+import React, { useState, lazy  } from 'react'
 import Layout from '@theme/Layout';
-import { Wheel } from 'react-custom-roulette'
-import ConfettiExplosion from 'react-confetti-explosion'
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import ConfettiExplosion from 'react-confetti-explosion';
 import { Formik, Form, Field, FieldArray } from 'formik'
 
-function getRandomRolor() {
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += Math.floor(Math.random() * 10);
-    }
-    return color;
-}
-
 function Raffler () {
-    const [mustSpin, setMustSpin] = useState(false);
-    const [prizeNumber, setPrizeNumber] = useState(0);
-    const [participants, setParticipants] = useState([{option: ''}, {option: ''}, {option: ''}]);
-    const [isExploding, setIsExploding] = React.useState(false);
-
-    const handleStartSpinningClick = (values) => {
-        if (!mustSpin) {
-            const newPrizeNumber = Math.floor(Math.random() * values.participants.length);
-            setParticipants(values.participants);
-            setPrizeNumber(newPrizeNumber);
-            setMustSpin(true);
-        }
-    }
-
     return (
         <Layout
             title="PHPUGMRN Raffler"
             description="Raffle Tool"
         >
             <main>
-                <div className="container margin-vert--lg">
-                    <div className="row">
-                        <div className="col col--3"></div>
-                        <div className="col col--9">
-                            <h2>PHPUGMRN Raffler</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col col--3"></div>
-                        <div className="col col--3">
-                            <Formik
-                                initialValues={{ participants: [{option: '', style: { backgroundColor: getRandomRolor()}}] }}
-                                onSubmit={handleStartSpinningClick}
-                                render={({ values }) => (
-                                    <Form>
-                                        <FieldArray
-                                            name="participants"
-                                            render={arrayHelpers => (
-                                                <div>
-                                                    <div className="margin-bottom--sm">
-                                                        <button className="button button--primary" type="submit">Let's go</button>
-                                                    </div>
-                                                    {(
-                                                        values.participants.map((friend, index) => (
-                                                            <div key={index}>
-                                                                <Field name={`participants.${index}.option`} />
-                                                                <button
-                                                                    type="button"
-                                                                    onClick={() => arrayHelpers.remove(index)} // remove participant from the list
-                                                                >
-                                                                    -
-                                                                </button>
-                                                                <button
-                                                                    type="button"
-                                                                    onClick={() => arrayHelpers.insert(index, {option: '', style: { backgroundColor: getRandomRolor()}})} // insert an empty string at a position
-                                                                >
-                                                                    +
-                                                                </button>
+                <BrowserOnly fallback={<div>Loading...</div>}>
+                    {() => {
+                        const [mustSpin, setMustSpin] = useState(false);
+                        const [prizeNumber, setPrizeNumber] = useState(0);
+                        const [participants, setParticipants] = useState([{option: ''}, {option: ''}, {option: ''}]);
+                        const [isExploding, setIsExploding] = React.useState(false);
+
+                        const Wheel = require('react-custom-roulette').Wheel;
+
+                        const handleStartSpinningClick = (values) => {
+                            if (!mustSpin) {
+                                const newPrizeNumber = Math.floor(Math.random() * values.participants.length);
+                                setParticipants(values.participants);
+                                setPrizeNumber(newPrizeNumber);
+                                setMustSpin(true);
+                            }
+                        }
+
+                        const getRandomRolor = ()=> {
+                            var color = "#";
+                            for (var i = 0; i < 6; i++) {
+                                color += Math.floor(Math.random() * 10);
+                            }
+                            return color;
+                        }
+
+                        return <div className="container margin-vert--lg">
+                            <div className="row">
+                                <div className="col col--3"></div>
+                                <div className="col col--9">
+                                    <h2>PHPUGMRN Raffler</h2>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col col--3"></div>
+                                <div className="col col--3">
+                                    <Formik
+                                        initialValues={{ participants: [{option: '', style: { backgroundColor: getRandomRolor()}}] }}
+                                        onSubmit={handleStartSpinningClick}
+                                        render={({ values }) => (
+                                            <Form>
+                                                <FieldArray
+                                                    name="participants"
+                                                    render={arrayHelpers => (
+                                                        <div>
+                                                            <div className="margin-bottom--sm">
+                                                                <button className="button button--primary" type="submit">Let's go</button>
                                                             </div>
-                                                        ))
+                                                            {(
+                                                                values.participants.map((friend, index) => (
+                                                                    <div key={index}>
+                                                                        <Field name={`participants.${index}.option`} />
+                                                                        <button
+                                                                            type="button"
+                                                                            onClick={() => arrayHelpers.remove(index)} // remove participant from the list
+                                                                        >
+                                                                            -
+                                                                        </button>
+                                                                        <button
+                                                                            type="button"
+                                                                            onClick={() => arrayHelpers.insert(index, {option: '', style: { backgroundColor: getRandomRolor()}})} // insert an empty string at a position
+                                                                        >
+                                                                            +
+                                                                        </button>
+                                                                    </div>
+                                                                ))
+                                                            )}
+                                                        </div>
                                                     )}
-                                                </div>
-                                            )}
-                                        />
-                                    </Form>
-                                )}
-                            />
+                                                />
+                                            </Form>
+                                        )}
+                                    />
+                                </div>
+                                <div className="col col--6">
+                                    {isExploding &&
+                                        <ConfettiExplosion
+                                            force={0.8}
+                                            duration={3000}
+                                            particleCount={250}
+                                            width={1600}
+                                            onComplete={() => {
+                                                setIsExploding(false);
+                                            }}
+                                        />}
+                                    <Wheel
+                                        mustStartSpinning={mustSpin}
+                                        prizeNumber={prizeNumber}
+                                        data={participants}
+                                        perpendicularText={true}
+                                        onStopSpinning={() => {
+                                            setMustSpin(false);
+                                            setIsExploding(true);
+                                        }}
+                                    />
+                                </div>
+                            </div>
                         </div>
-                        <div className="col col--6">
-                            {isExploding &&
-                                <ConfettiExplosion
-                                    force={0.8}
-                                    duration={3000}
-                                    particleCount={250}
-                                    width={1600}
-                                    onComplete={() => {
-                                        setIsExploding(false);
-                                    }}
-                                />}
-                            <Wheel
-                                mustStartSpinning={mustSpin}
-                                prizeNumber={prizeNumber}
-                                data={participants}
-                                perpendicularText={true}
-                                onStopSpinning={() => {
-                                    setMustSpin(false);
-                                    setIsExploding(true);
-                                }}
-                            />
-                        </div>
-                    </div>
-                </div>
+                    }}
+                </BrowserOnly>
             </main>
         </Layout>
     );
